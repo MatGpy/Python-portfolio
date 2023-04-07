b7fe1ef75ae036bac26b8b8abe5dd1cd25f81509
Mateusz <ganski213@gmail.com>
2023-04-07 23:49:08 +0200

    dodane komentarze do plikow palindromy.py i papier-kamien-nozyce.py

--- palindromy.py
+++ palindromy.py
@@ -1,18 +1,23 @@
+# Program sprawdzajacy czy dana liczba jest palindromem
+# program definiuje funkcje sprawdzajaca czy liczba jest palindromem
 def palindrom(x):
+    # funkcja zamienia liczbe podana na wejsciu na stringa
     y = str(x)
     m = 0
     i = 0
     l = len(y)-1
+    # funkcja posluguje sie petla while, aby sprawdzic, czy znaki po przeciwnych stronach stringa sa identyczne. Gdy tak jest, iterator zwiekszany jest o 1, tak samo jak waryosc zmiennej m
     while y[i] == y[l-i]:
           m = m+1
           i = i+1
+          # program konczy dzialanie petli w przypadku, w którym iterator osiaga wartosc wyzsza niz dlugosc badanego stringa
           if i > l:
              break
-
+    # funkcja sprawdza, czy wartosc zmiennej m jest rowna dlugosci badanego stringa (wartosc m byla zwiekszana o 1 za kazdym razem, kiedy znaki po przeciwnych stronach stringa byly identyczne. Dlatego zakladajac, ze petla przeszla przez calego stringa, liczba w stringu jest palindromem wylacznie wtedy, gdy wartosc m jest rowna dlugosci tego stringa)
     if m == len(y):
        print("is a palindrome")
     else:
        print("isnt a palindrome")
-       
+# program pobiera od uzytkownika liczbe, ktora uzytkownik chce zbadac, a nastepnie zwraca odpowiednia odpowiedz w zaleznosci od tego, czy podana liczba jest palindromem      
 number = int(input("type a number: \n"))
 print(palindrom(number))
--- papier-kamien-nozyce.py
+++ papier-kamien-nozyce.py
@@ -1,5 +1,10 @@
+# Program do gry w papier kamien nozyce. Program dziala tak, ze uzytkownik wybiera swoj ruch, potem program. Nastepnie ruchy gracza i programu sa porownywane i wyswietlany jest wynik
 import random
-v1 = str(input("Now it's your turn: "))
+# program pobiera od uzytkownika jego ruch i zwraca blad, jesli jego ruch jest niezgodny z zasadami gry
+v1 = str(input("Now it's your turn (you can chose rock, paper, or scissors): "))
+if v1 != "rock" and v1 != "paper" and v1 != "scissors":
+    raise TypeError
+# program wybiera swoj ruch w oparciu o wartosc losowo wygenerowanej liczby
 v2 = random.randint(1,3)
 def aaa(x):
     if x == 1:
@@ -8,6 +13,7 @@ def aaa(x):
         return "paper"
     if x == 3:
         return "scissors"
+# program tworzy funkcje do porownywania ruchow gracza i programu
 def bbb(x, y):
     if x == "rock" and y == "scissors" or x == "paper" and y == "rock" or x == "scissors" and y == "paper":
         return "player 2 won"
@@ -15,4 +21,5 @@ def bbb(x, y):
         return "you won"
     if x == "rock" and y == "rock" or x == "paper" and y == "paper" or x == "scissors" and y == "scissors":
         return "draw"
+# program porownuje ruch gracza z ruchem programu za pomoca uprzednio utworzonej funkcji i pokazuje wynik starcia wraz z ruchem programu
 print("player 2: {}\n{}".format(aaa(v2), bbb(aaa(v2), v1)))
