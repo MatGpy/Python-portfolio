1222bbf551665a33642df9d350af4b12b02719b2
MatGpy <129793553+MatGpy@users.noreply.github.com>
2023-04-25 19:18:25 +0200

    Zmiany w pliku palindromy.py - od teraz sprawdzane sa i liczby, i slowa

--- palindromy.py
+++ palindromy.py
@@ -1,23 +1,21 @@
-# Program sprawdzajacy czy dana liczba jest palindromem
-# program definiuje funkcje sprawdzajaca czy liczba jest palindromem
+# Program sprawdzajacy czy dana liczba/slowo jest palindromem
+# program definiuje funkcje sprawdzajaca czy liczba/slowo jest palindromem
 def palindrom(x):
-    # funkcja zamienia liczbe podana na wejsciu na stringa
-    string1 = str(x)
     j = 0
     i = 0
-    length = len(string1)-1
+    length = len(x)-1
     # funkcja posluguje sie petla while, aby sprawdzic, czy znaki po przeciwnych stronach stringa sa identyczne. Gdy tak jest, iterator zwiekszany jest o 1, tak samo jak waryosc zmiennej m
-    while string1[i] == string1[length-i]:
+    while x[i] == x[length-i]:
           j = j+1
           i = i+1
-          # program konczy dzialanie petli w przypadku, w którym iterator osiaga wartosc wyzsza niz dlugosc badanego stringa
+          # program konczy dzialanie petli w przypadku, w ktorym iterator osiaga wartosc wyzsza niz dlugosc badanego stringa
           if i > length:
              break
     # funkcja sprawdza, czy wartosc zmiennej m jest rowna dlugosci badanego stringa (wartosc m byla zwiekszana o 1 za kazdym razem, kiedy znaki po przeciwnych stronach stringa byly identyczne. Dlatego zakladajac, ze petla przeszla przez calego stringa, liczba w stringu jest palindromem wylacznie wtedy, gdy wartosc m jest rowna dlugosci tego stringa)
-    if j == len(string1):
-       return "your number is a palindrome"
+    if j == len(x):
+       return "{} is a palindrome".format(x)
     else:
-       return "your number isn't a palindrome"
-# program pobiera od uzytkownika liczbe, ktora uzytkownik chce zbadac, a nastepnie zwraca odpowiednia odpowiedz w zaleznosci od tego, czy podana liczba jest palindromem      
-number = int(input("type a number: \n"))
-print(palindrom(number))
+       return "{} isn't a palindrome".format(x)
+# program pobiera od uzytkownika liczbe/slowo, ktora uzytkownik chce zbadac, a nastepnie zwraca odpowiednia odpowiedz w zaleznosci od tego, czy podana liczba/slowo jest palindromem      
+input = str(input("type a number/word: \n"))
+print(palindrom(input))
