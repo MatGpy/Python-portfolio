c41632d34c2195797031a2e243403bfab2f2c02d
MatGpy <129793553+MatGpy@users.noreply.github.com>
2023-04-29 18:30:00 +0200

    usprawniony system przyporzadkowywania poziomu trudnosci w pliku zgadywanie numeru.py

--- zgadywanie numeru.py
+++ zgadywanie numeru.py
@@ -7,19 +7,15 @@ num2 = random.randint(1,3)
 num3 = random.randint(1,10)
 # program tworzy zmienna przeznaczona do przechowywania ilosci prob dostepnych dla gracza
 level = 0
+# program tworzy slownik do przechowywania dostepnych poziomow trudnosci i odpowiadajacych im ilosci prob
+level1 = {"very easy":8, "easy":6, "normal":4, "hard":2}
 # program pyta uzytkownika o poziom trudnosci, z ktorym chce on grac i daje mu konkretna ilosc prob w zaleznosci od wybranego poziomu trudnosci. Jesli uzytkownik poda poziom trudnosci inny niz podane do wyboru, program zwraca blad
 print("levels of difficulty:\nvery easy - 8 attempts\neasy - 6 attempts\nnormal - 4 attempts\nhard - 2 attempts")
 level2 = str(input("Choose level of difficulty: "))
-if level2 == "very easy":
-    level = 8
-if level2 == "easy":
-    level = 6
-if level2 == "normal":
-    level = 4
-if level2 == "hard":
-    level = 2
 if level2 != "very easy" and level2 != "easy" and level2 != "normal" and level2 != "hard":
     raise ValueError("player chose incorrect level of difficulty")
+else:
+    level = level1[level2]
 # program pyta sie uzytkownika o to, czy chce on dostac wskazowke i daje mu ja, jesli uzytkownik tego chce
 hint = str(input("Do you want to get a hint? "))
 if hint == "yes":
