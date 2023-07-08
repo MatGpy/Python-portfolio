fbce216689315f58a9f3a41ed153c30646520e88
MatGpy <129793553+MatGpy@users.noreply.github.com>
2023-07-08 19:01:54 +0200

    full english translation of all projects

--- prime_numbers.py
+++ prime_numbers.py
@@ -1,24 +1,18 @@
-# This program checks if number from the input is ab prime number
+# This program checks if number from the input is a prime number
 # program takes a number from user's input
-'''
-number = int(input("insert a number: "))
+from math import sqrt
+number = float(input("insert a number: "))
 # program raises ValueError if number from the input is negative
 if number < 0:
    raise ValueError("incorrect data")
-# program tworzy funkcje sprawdzajaca, czy dana liczba jest liczba jest liczba pierwsza. Jej dzialanie opiera sie na zaleznosci, ktora zauwazylem - otoz kazda liczba zlozona musi dzielic sie bez reszty przez 2, 3 lub 5 - jesli dana liczba nie dzieli sie przez zadna z nich, to jest ona liczba pierwsza
-#  program creates a function which checks if certain number is a 
+# program creates a function which checks if certain number is a prime number - it checks if this number can be divided by numbers between 2 and square root of this number
 def check_if_prime(x):
-    if x % 2 != 0 and x % 3 != 0 and x % 5 != 0:
-       return("your number is a prime number")
-    else:
-       return("your number isn't a prime number")
-# program sprawdza, czy liczba podana przez uzytkownika jest rowna 0,1,2,3, albo 5 - w takich przypadkach funkcja zwrocilaby nieprawidlowe wyniki, dlatego dla tych przypadkow trzeba indywidualnie przypisac dzialanie programu. Po analizie tej liczby program odpowiada, czy jest ona pierwsza, czy zlozona 
-if number == 2 or number == 3 or number == 5:
-   print("your number is a prime number")
-elif number == 1 or number == 0:
-   print("your number isn't a prime number")
+   for i in range(2, round(sqrt(x)+1)):
+       if x%i == 0:
+         return("isn't a prime number")
+   return("is a prime number")
+# program checks if numer from user's input is 0 or 1 (these numbers aren't prime numbers) and if it isn't, program uses a function created previously to check if this number is a prime number
+if number == 0 or number == 1:
+   print("isn't a prime number")
 else:
    print(check_if_prime(number))
-   '''
-
-# this code is under construction
